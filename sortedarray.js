70c0615f371246f381475963ab8f067573cfe4ca
JEEVITHASRINI97 <77686700+JEEVITHASRINI97@users.noreply.github.com>
2022-12-26 10:08:36 +0530

    update sortedArray

--- sortedarray.js
+++ sortedarray.js
@@ -40,42 +40,4 @@
     //OUTPUT: 20.5
     
    
-    //h.Rotate an array k times
-    
-    //Anonymous :
-    
-    
-    let rotateArray =function(A, K) {
-      if (A.length === K || K === 0) {
-          return A;
-      }
-      K = K % A.length;
-      for (let i = 0; i < K; i++) {
-          A.unshift(A.pop());
-      }
-    console.log(A)
-    }
-    rotateArray([1,2,3,4,5],2)
-    
-    
-    //OUTPUT: (5) [4, 5, 1, 2, 3]
-    
-    
-    //IIFE :
-    
-    (function(A, K) {
-      if (A.length === K || K === 0) {
-          return A;
-      }
-      K = K % A.length;
-      for (let i = 0; i < K; i++) {
-          A.unshift(A.pop());
-      }
-    console.log(A)
-    })([1,2,3,4,5],2)
-    
-    //OUTPUT: (5) [4, 5, 1, 2, 3]
-    
-    
-    
     
